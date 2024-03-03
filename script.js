aee471248dff58db8caaf68dd00d7013bfce8570
miwanoff <marina.iva.noffa@gmail.com>
2024-03-03 12:43:31 +0200

    Add slideanim

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 
 (function ($, undefined) {
   $(window).scroll(function () {
+     // якщо відбувається скролінг
     /* Скроллировать фоновые слои с разной скоростью */
     let winTop = $(window).scrollTop();
     $(".parallax").css(
@@ -22,5 +23,20 @@
         (-20 - winTop * 0.5) +
         "px"
     );
+    if ($(window).scrollTop() > 200) {
+      $(".navbar").css("opacity", "0.5");
+    } else {
+      $(".navbar").css("opacity", "1");
+    }
+
+   
+    $(".slideanim").each(function () {
+      // для кожного блоку з класом slideanim
+      let pos = $(this).offset().top; // считываем его координату по оси Y в окне браузера
+      if (pos < $(window).scrollTop() + 400) {
+        //якщо до верху сторінки залишається 400px,
+        $(this).addClass("slide"); //додаємо до блоку клас slide з анімацією
+      }
+    });
   });
 })(jQuery);
